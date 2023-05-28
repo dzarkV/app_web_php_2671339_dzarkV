891515111da2cbd1dbe49b165db81b4f02e7fb0a
ProfeAlbeiro <profealbeiro2020@gmail.com>
2023-05-28 10:29:11 -0500

    ajustando el proyecto

--- controllers/Login.php
+++ controllers/Login.php
@@ -1,28 +1,17 @@
-<?php
-    require_once "models/model_dto/UserDto.php";
+<?php    
     class Login{
         public function __construct(){}
         public function main(){
             if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-                require_once "views/roles/business/header.view.php";
-                require_once "views/business/login.view.php";
-                require_once "views/roles/business/footer.view.php";
+                require_once "views/company/header.view.php";
+                require_once "views/company/login.view.php";
+                require_once "views/company/footer.view.php";
             }
             if ($_SERVER['REQUEST_METHOD'] == 'POST') {                
-                // Capturar los datos a través de un objeto
-                $userDto = new UserDto(
-                    $_POST['user'], 
-                    $_POST['pass']
-                );
-                // Validar la entrada por contraseña
-                if ($userDto->getIdUser() == "admin-123" && $userDto->getPassUser() == "12345") {                    
-                    header('Location: ?c=Dashboard');
-                } else {
-                    require_once "views/roles/business/header.view.php";
-                    require_once "views/business/login.view.php";
-                    echo "Usuario Incorrecto";
-                    require_once "views/roles/business/footer.view.php";
-                }                
+                $user = $_POST['user'];
+                $pass = $_POST['pass'];
+                echo "Usuario: " . $user . "<br>";
+                echo "Contraseña: " . $pass . "<br>";
             }
         }
     }    
