3d3cef83da62030e624abc8a0d5dd038373e2951
ProfeAlbeiro <profealbeiro2020@gmail.com>
2023-05-28 10:02:48 -0500

    ajustando el proyecto

--- controllers/Landing.php
+++ controllers/Landing.php
@@ -2,9 +2,9 @@
     class Landing{
         public function __construct(){}
         public function main(){
-            require_once "views/roles/business/header.view.php";
-            require_once "views/roles/business/index.view.php";
-            require_once "views/roles/business/footer.view.php";
+            require_once "views/company/header.view.php";
+            require_once "views/company/index.view.php";
+            require_once "views/company/footer.view.php";
         }
     }    
 ?>
