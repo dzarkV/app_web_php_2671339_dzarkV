900cfaa808495ed629c6551f4cecad43e7cc6a29
ProfeAlbeiro <profealbeiro2020@gmail.com>
2023-06-04 08:45:20 -0500

    Iniciando clase

--- assets/docs/trim2/3_db/dbejemplouser.sql
+++ assets/docs/trim2/3_db/dbejemplouser.sql
@@ -1,16 +1,34 @@
+DROP DATABASE DBEJEMPLOUSER;
 -- -----------------------------------------------------
--- ESTRUCTURA dbejemplouser
+-- ESTRUCTURA DBEJEMPLOUSER
 -- -----------------------------------------------------
-CREATE DATABASE dbejemplouser DEFAULT CHARACTER SET utf8 ;
-USE dbejemplouser ;
+CREATE DATABASE DBEJEMPLOUSER DEFAULT CHARACTER SET utf8 ;
+USE DBEJEMPLOUSER ;
 
 -- -----------------------------------------------------
--- TABLA USER
+-- TABLA ROLES
 -- -----------------------------------------------------
-CREATE TABLE USER (
-  user_code INT NOT NULL AUTO_INCREMENT,
-  user_name VARCHAR(150) NOT NULL,
-  user_email VARCHAR(150) NOT NULL,
-  user_pass VARCHAR(150) NOT NULL,
-  PRIMARY KEY (user_code)
+CREATE TABLE ROLES (
+  rol_code INT NOT NULL AUTO_INCREMENT,
+  rol_name VARCHAR(50) NOT NULL,
+  PRIMARY KEY (rol_code)
+) ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- TABLA USERS
+-- -----------------------------------------------------
+CREATE TABLE USERS (
+  rol_code INT NOT NULL,
+  user_code VARCHAR(100) NOT NULL,
+  user_name VARCHAR(50) NOT NULL,
+  user_lastname VARCHAR(50) NOT NULL,
+  user_email VARCHAR(100) NOT NULL,
+  PRIMARY KEY (user_code),
+  INDEX ind_user_rol (rol_code ASC),
+  CONSTRAINT fk_user_rol
+    FOREIGN KEY (rol_code)
+    REFERENCES ROLES (rol_code)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 ) ENGINE = InnoDB;
