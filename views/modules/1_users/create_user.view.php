fe99626ada5a25f9a383c476edd7cdef848687e5
ProfeAlbeiro <profealbeiro2020@gmail.com>
2023-06-17 16:36:31 -0500

    Ajustando Proyecto

--- views/modules/1_users/create_user.view.php
+++ views/modules/1_users/create_user.view.php
@@ -4,8 +4,51 @@
     </div>
     <div class="form-body">
         <div class="form-control">
-            <label for="userName">Nombre Usuario</label>
-            <input type="text" name="userName" id="userName" placeholder="Nombre Rol">
+            <label for="rolCode">Rol</label>
+            <select name="rolCode" id="rolCode">
+                <option value="3">seller</option>
+                <option value="2">customer</option>
+                <option value="1">admin</option>
+            </select>
+        </div>
+        <div class="form-control">
+            <label for="userCode">Código</label>
+            <input type="text" name="userCode" id="userCode" placeholder="Código Usuario">
+        </div>
+        <div class="form-control">
+            <label for="userId">Identificación</label>
+            <input type="number" name="userId" id="userId" placeholder="Identificación">
+        </div>
+        <div class="form-control">
+            <label for="userName">Nombres</label>
+            <input type="text" name="userName" id="userName" placeholder="Nombres">
+        </div>
+        <div class="form-control">
+            <label for="userLastName">Apellidos</label>
+            <input type="text" name="userLastName" id="userLastName" placeholder="Apellidos">
+        </div>
+        <div class="form-control">
+            <label for="userEmail">Email</label>
+            <input type="email" name="userEmail" id="userEmail" placeholder="Email">
+        </div>
+        <div class="form-control">
+            <label for="userPhone">Celular</label>
+            <input type="text" name="userPhone" id="userPhone" placeholder="Celular">
+        </div>
+        <div class="form-control">
+            <label for="userPass">Contraseña</label>
+            <input type="password" name="userPass" id="userPass" placeholder="Contraseña">
+        </div>
+        <div class="form-control">
+            <label for="userConfirmPass">Confirmación</label>
+            <input type="password" name="userConfirmPass" id="userConfirmPass" placeholder="Confirmación">
+        </div>
+        <div class="form-control">
+            <label for="userStatus">Estado</label>
+            <select name="userStatus" id="userStatus">                
+                <option value="1">Activo</option>
+                <option value="0">Inactivo</option>
+            </select>
         </div>
     </div>
     <div class="form-footer">
