2b9a11709c09ba9e1b065ad08ba206ff8c2c5765
ProfeAlbeiro <profealbeiro2020@gmail.com>
2023-06-18 11:43:01 -0500

    Iniciando clase

--- controllers/Landing.php
+++ controllers/Landing.php
@@ -6,5 +6,25 @@
             require_once "views/company/index.view.php";
             require_once "views/company/footer.view.php";
         }
+        public function about(){
+            require_once "views/company/header.view.php";            
+            require_once "views/company/about.view.php";
+            require_once "views/company/footer.view.php";
+        }
+        public function shop(){
+            require_once "views/company/header.view.php";            
+            require_once "views/company/shop.view.php";
+            require_once "views/company/footer.view.php";
+        }
+        public function shopSingle(){
+            require_once "views/company/header.view.php";            
+            require_once "views/company/shop-single.view.php";
+            require_once "views/company/footer.view.php";
+        }
+        public function contact(){
+            require_once "views/company/header.view.php";            
+            require_once "views/company/contact.view.php";
+            require_once "views/company/footer.view.php";
+        }
     }    
 ?>
