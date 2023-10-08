deb5aca8c99a486abb64e21a73bad044ae0ff881
ProfeAlbeiro <profealbeiro2020@gmail.com>
2023-10-08 12:12:09 -0500

    Creando servicio db en azure y modificando modelo de conexión pdo

--- models/DataBase.php
+++ models/DataBase.php
@@ -1,14 +1,31 @@
 <?php
     class DataBase{
+        
+        # Conexión Externa
         public static function connection(){            
-            $hostname = "localhost";
+            $hostname = "dbappwebphp2671339.mysql.database.azure.com";
             $port = "3306";
             $database = "dbejemplouser";
-            $username = "root";
-            $password = "";
-			$pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password);
+            $username = "adminappwebphp";
+            $password = "Arv12345";
+            $options = array(
+                PDO::MYSQL_ATTR_SSL_CA => 'assets/docs/DigiCertGlobalRootCA.crt.pem'
+            );
+			$pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password,$options);
 			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			return $pdo;
 		}
 	}
+        #  Conexión Local
+        // public static function connection(){            
+        //     $hostname = "localhost";
+        //     $port = "3306";
+        //     $database = "dbejemplouser";
+        //     $username = "root";
+        //     $password = "";
+		// 	$pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password);
+		// 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		// 	return $pdo;
+		// }
+	// }
 ?>
