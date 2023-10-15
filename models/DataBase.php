27f40b39e44cfa3346d9605b27f52166a087de14
Diego Vargas <91356068+dzarkV@users.noreply.github.com>
2023-10-15 04:04:16 +0000

    variables entorno de azure conection

--- models/DataBase.php
+++ models/DataBase.php
@@ -3,19 +3,15 @@
         
         # Conexión Externa
         public static function connection(){            
-            $hostname = getenv("DB_HOSTNAME");
-            $port = "3306";
-            $database = getenv("DB_NAME");
-            $username = getenv("DB_USERNAME");
-            $password = getenv("DB_PASSWORD");
-            // $options = array(
-            //     PDO::MYSQL_ATTR_SSL_CA => 'assets/docs/DigiCertGlobalRootCA.crt.pem' 
-            // );
-            // $options = array(
-            //     PDO::MYSQL_ATTR_SSL_CA => 'models/DigiCertGlobalRootCA.crt.pem' 
-            // );
-			$pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password
-        );
+            $hostname = getenv("AZURE_MYSQL_HOST");
+            $port = getenv("AZURE_MYSQL_PORT");
+            $database = getenv("AZURE_MYSQL_DBNAME");
+            $username = getenv("AZURE_MYSQL_USERNAME");
+            $password = getenv("AZURE_MYSQL_PASSWORD");
+            $options = array(
+                PDO::MYSQL_ATTR_SSL_CA => 'assets/docs/DigiCertGlobalRootCA.crt.pem' 
+            );
+			$pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password, $options);
 			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			return $pdo;
 		}
